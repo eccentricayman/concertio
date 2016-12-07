edc2677f976b21e50d7cccb50ed00917423bfed4
EMilborn <emilborn@stuy.edu>
2016-12-07 12:35:11 -0500

    - ticketmaster, + jambase

---
+++ utils/jambase.py
@@ -0,0 +1,11 @@
+#626c4n8t8x4dy4qxs5jfuy4k
+
+def events(search):
+
+def link(event):
+
+def loc(event):
+
+def artists(event):
+
+def images(event):
---
+++ utils/lastfm.py
@@ -0,0 +1,11 @@
+
+
+
+def toptracks (artist):
+    #return an array of the top tracks
+
+def image (artist):
+    #returns last.fm's picture for the artist
+    
+def link (artist):
+    #returns url for an artist's last.fm page
