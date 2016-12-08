a7316cfe1f4f312ad41e14a77284cc9a49a1a28b
Elias Milborn <emilborn@stuy.edu>
2016-12-07 21:59:37 -0500

    started work on jambase

--- utils/jambase.py
+++ utils/jambase.py
@@ -1,11 +1,38 @@
 #626c4n8t8x4dy4qxs5jfuy4k
+import urllib2
+import xml.etree.ElementTree as ET
 
-def events(search):
+#def events(search):
+    
 
-def link(event):
+def eventsHelp(z, artist):
+    u = "http://api.jambase.com/events?api_key=626c4n8t8x4dy4qxs5jfuy4k"
+    if z != null:
+        u += "&zipCode=" + z + "&radius=0" #radius zero so it is only in that zipCode
+        
+    if artist != null:
+        artist = artistId(artist)
+        u += "&artistId=" + artist
 
-def loc(event):
+    u = urllib2.urlopen(u)
+    u = u.read()
+    #need to learn how to use xml
 
-def artists(event):
 
-def images(event):
+        
+
+def artistId(artist):
+    u = "http://api.jambase.com/artists?api_key=626c4n8t8x4dy4qxs5jfuy4k&name=" + artist
+    u = urllib2.urlopen(u)
+    u = u.read()
+    #need to learn how to use xml
+    return "8317"
+
+#def link(event):
+
+#def loc(event):
+
+#def artists(event):
+    
+#def images(event):
+
