63bdd96607eb873c05afab70831b3cb4ded7a403
ayu7 <ayu7@stuy.edu>
2016-12-08 12:30:34 -0500

    changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
-from jambase import events
+from utils import jambase
+
+app=Flask(__name__)
 
 @app.route("/")
 def home():
@@ -7,20 +9,24 @@ def home():
 
 @app.route("/search", methods=["POST"])
 def search ():
-    userinput= request.form["search"]
-    if (userinput==""):
+    userInput= request.form["search"]
+    if (userInput==""):
         return render_template("home.html", message="Please search for something")
-    results(jambase.events(userinput))
+    results(jambase.events(userInput))
+    ## events from jambase should return a dictionary in format of {event0ID:[link, artist, etc], event1ID:[same]}
 
 @app.route("/results")
-def results():
-    render_template("results.html")
+def results(search):
+    render_template("results.html", results=search)
+    ##sends dictionary to html, go thru each entry and format?
 
+##click on some link on results page? searches using jambase for specific event info    
 @app.route("/event", methods=["GET"])
 def event():
-    render_template("event.html")
+    eventID=request.form['event']
+    eventInfo=results(jambase.events(eventID))
+    render_template("event.html",event=eventInfo)
     
-
 
 if __name__=="__main__":
     app.debug=True
--- utils/jambase.py
+++ utils/jambase.py
@@ -1,11 +1,12 @@
 #626c4n8t8x4dy4qxs5jfuy4k
 
 def events(search):
-
+    return ""
 def link(event):
-
+    return ""
 def loc(event):
-
+    return ""
 def artists(event):
-
+    return ""
 def images(event):
+    return ""
