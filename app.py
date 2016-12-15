08865c9b8e7944161d333ed872da40e1f6f77cfe
ayu7 <ayu7@stuy.edu>
2016-12-15 05:03:34 -0500

    deleted login stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, session
-from utils import jambase, musixmatch, dynamicsearch, login
+from utils import jambase, musixmatch, dynamicsearch
 import os
 
 app = Flask(__name__)
@@ -7,32 +7,7 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
-    try:
-        return render_template("home.html", loggedIn = session['user'])
-    except KeyError:
-        return render_template("home.html", loggedIn = False)
-##logged in is a boolean user is true if user is logged in
-## used in html to check if register/login buttons should show up
-
-@app.route("/authenticate")
-def log():
-    user=request.form["username"]
-    password=request.form["password"]
-    whichButton = request.form["submitButton"]
-    if (whichButton == "Login"):
-        authen=login.login(user, password)
-        if authen=="":
-            session['user']=True
-            hasError = False
-        else:
-            hasError = True
-    if (whichButton == "Register"):
-        authen = login.register(user, password)
-        if authen == "":
-            hasError = False
-        else:
-            hasError = True
-    return render_template("home.html", message=authen, error = hasError)
+    return render_template("home.html")
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -54,11 +29,6 @@ def event():
     eventID = request.args['eventID'] ##value of form/link should be event id or event name or something
     eventInfo = jambase.eventData(eventID) ##waiting on jambase.py for specific formatting
     return render_template("event.html",event = eventInfo)
-
-@app.route("/logout")
-def logout():
-    session['user']=False
-    redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.debug = True
